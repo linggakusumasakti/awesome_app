81510dc2053a99cf0907db6221d9832c7cd5c58d
linggakusumasakti <linggakusumasakti@gmail.com>
2024-12-04 12:18:43 +0700

    implement movie api client test

--- test/data/api/movie_api_client_test.dart
+++ test/data/api/movie_api_client_test.dart
@@ -1,4 +1,5 @@
 import 'package:awesome_app/data/api/movie_api_client.dart';
+import 'package:awesome_app/data/models/movie.dart';
 import 'package:awesome_app/utils/constants.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
@@ -14,10 +15,6 @@ void main() {
   group('get movies', () {
     late http.Client httpClient;
     late MovieApiClient movieApiClient;
-    final headers = {
-      'Authorization': apiKey,
-      'Content-Type': 'application/json',
-    };
 
     setUpAll(() {
       registerFallbackValue(FakeUri());
@@ -37,7 +34,7 @@ void main() {
       } catch (_) {}
       verify(
         () => httpClient.get(
-          headers: headers,
+          headers: any(named: 'headers'),
           Uri.https(
             baseUrl,
             '/3/movie/now_playing',
@@ -47,6 +44,53 @@ void main() {
           ),
         ),
       ).called(1);
+    });
+
+    test('throws movies failure on non-200 response', () async {
+      final response = MockResponse();
+      when(() => response.statusCode).thenReturn(400);
+      when(() => httpClient.get(any(), headers: any(named: 'headers')))
+          .thenAnswer((_) async => response);
+      expect(
+        () async => movieApiClient.getMovies(),
+        throwsA(isA<Exception>()),
+      );
+    });
+
+    test('return list movies valid response', () async {
+      final response = MockResponse();
+      when(() => response.statusCode).thenReturn(200);
+      when(() => response.body).thenReturn(
+        '''
+{
+  "results": [
+        {
+            "adult": false,
+            "backdrop_path": "/tElnmtQ6yz1PjN1kePNl8yMSb59.jpg",
+            "genre_ids": [
+                16,
+                12,
+                10751,
+                35
+            ],
+            "id": 1241982,
+            "original_language": "en",
+            "original_title": "Moana 2",
+            "overview": "After receiving an unexpected call from her wayfinding ancestors, Moana journeys alongside Maui and a new crew to the far seas of Oceania and into dangerous, long-lost waters for an adventure unlike anything she's ever faced.",
+            "popularity": 6106.764,
+            "poster_path": "/yh64qw9mgXBvlaWDi7Q9tpUBAvH.jpg",
+            "release_date": "2024-11-27",
+            "title": "Moana 2",
+            "video": false,
+            "vote_average": 7.0,
+            "vote_count": 273
+        }]
+}''',
+      );
+      when(() => httpClient.get(any(), headers: any(named: 'headers')))
+          .thenAnswer((_) async => response);
+      final movies = await movieApiClient.getMovies();
+      expect(movies, isA<List<Movie>>());
     });
   });
 }
